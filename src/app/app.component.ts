f265d60f08095b05de18ae43fe123faa5915933c
Jared Johnson <jaredjohnson@Jareds-MacBook-Pro.local>
2024-06-10 19:45:11 -0400

    adding routing capabilities and component declarations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LandingComponent } from './landing/landing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, LandingComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './landing/landing.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'landing',
+        component: LandingComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: '',
+        redirectTo: 'landing',
+        pathMatch:'full'
+      },
+      {
+        path: '**',
+        redirectTo: 'landing',
+        pathMatch: 'full'
+      }
+];
