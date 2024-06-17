113d026b58bf3880d241e1359003542170d0bcc6
Jared Johnson <jaredjohnson@Jareds-MacBook-Pro.local>
2024-06-16 22:55:07 -0400

    working on gaming page and also player options and styling for youtube

--- src/app/gaming/gaming.component.ts
+++ src/app/gaming/gaming.component.ts
@@ -1,14 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { YouTubePlayer } from '@angular/youtube-player';
 
 @Component({
   selector: 'app-gaming',
   standalone: true,
-  imports: [],
+  imports: [NavbarComponent, YouTubePlayer],
   templateUrl: './gaming.component.html',
 })
-export class GamingComponent {
+
+
+
+export class GamingComponent implements OnInit, AfterViewInit {
+
+
+  @ViewChild('youtubePlayer')youtubePlayerVariable!: ElementRef;
+
+  videoHeight: number | undefined;
+  videoWidth: number | undefined;
+
+
 
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit(): void {
+    console.log('yt variable', this.youtubePlayerVariable);
+  }
+
+  onResize() {
 
   }
 }
